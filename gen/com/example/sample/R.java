0a15b25d35c2c120340dc18d5871c0f2e24bab43
javier <jaynav@outlook.com>
2014-01-09 21:49:59 -0800

    minor learning change

--- gen/com/example/sample/R.java
+++ gen/com/example/sample/R.java
@@ -27,9 +27,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090002;
+        public static final int action_settings=0x7f090004;
         public static final int derText=0x7f090001;
         public static final int helpText=0x7f090000;
+        public static final int seekBar1=0x7f090002;
+        public static final int seeksBarText=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -40,6 +42,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int invalues_seekbar=0x7f060006;
         public static final int invalues_string_button=0x7f060003;
         public static final int invalues_string_complain=0x7f060005;
         public static final int invalues_string_sayText=0x7f060002;
--- src/com/example/sample/MainActivity.java
+++ src/com/example/sample/MainActivity.java
@@ -4,23 +4,61 @@ package com.example.sample;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
-import android.widget.Toast;//retarded name for message box
+import android.widget.Toast;
 import android.content.Context;
 import android.content.Intent;//retarded name
 
 public class MainActivity extends Activity {
-
+ private SeekBar derSeekBar;
+ private EditText derText;
+ private int iCount =0;
     @Override
+   
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        /////////////////////// creating listener for seekbar
+        derSeekBar = (SeekBar)findViewById(R.id.seekBar1);
+        derText = (EditText)findViewById(R.id.seeksBarText);
+        final TextView helper1 = (TextView) findViewById(R.id.helpText);
+        derSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+        	//requires all three
+        	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+        		{
+        			//retard code does not take a int and automatically convert it to a string or have a progress.toString() method.
+        			//note to self this is not C# or jvm. its dalvik java
+                     helper1.setText(Integer.toString(progress));
+        			//helper1.setText(String.valueOf(progress));
+                    
+        		}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0)
+				{
+					// TODO Auto-generated method stub
+					
+				}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0)
+				{
+					// TODO Auto-generated method stub
+					
+				}
+
+});
+        
     }
 
 
@@ -31,7 +69,37 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    @Override
+    public void onResume()
+    	{
+    		Log.v("its in mainActivity","onResume");
+    		super.onResume();
+    	}
+    @Override
+    public void onPause()
+    	{
+    		Log.v("its in mainActivity","onPause");
+    		super.onPause();
+    	}
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    	{
+    		Log.v("its in mainActivity","onSaveInstance");	
+    		super.onSaveInstanceState(outState);
+    		
+    	
+    	}
+    @Override
+    public void onDestroy()
+    	{
+    		Log.v("its in mainActivity","onDestroy"); 
+    		super.onDestroy();
+    	}
     
+    
+    
+    
+    ////////////////////////////////////////////////end of android states///////////////////////////////////////////////////
     public void openURL(View der)
     {
     	EditText derurl = (EditText) findViewById(R.id.derText);
@@ -55,24 +123,23 @@ public class MainActivity extends Activity {
     				   
     			   }
 
-				@Override
-				public void onAnimationEnd(Animation arg0) {
+    			   @Override
+    			   public void onAnimationEnd(Animation arg0) {
 					
-					helper.setText("");
-					Context cont = getApplicationContext();
-					CharSequence daText = "herp Derp";
-					int time = Toast.LENGTH_SHORT;
-					Toast quickmessagebox = Toast.makeText(cont, daText, time);
-					quickmessagebox.show();
-					
-				}
+    				   helper.setText("");
+    				   Context cont = getApplicationContext();
+    				   CharSequence daText = "herp Derp";
+    				   int time = Toast.LENGTH_SHORT;
+    				   Toast quickmessagebox = Toast.makeText(cont, daText, time);
+    				   quickmessagebox.show();
+    			   		}
 
-				@Override
-				public void onAnimationRepeat(Animation animation) {
+    			   @Override
+    			   public void onAnimationRepeat(Animation animation) {
 					
-				}
+    			   }
     		   });
-       helper.startAnimation(animate);
+       		helper.startAnimation(animate);
     }
     
 }
